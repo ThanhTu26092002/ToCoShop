a24b01ef91028c6daaf3cda92e07aee4d3e1015e
ThanhTu26092002 <lethanhtu810@email.com>
2022-12-06 13:49:11 +0700

    update to employees

--- backend/src/v1/index.js
+++ backend/src/v1/index.js
@@ -7,7 +7,7 @@ routes.use('/categories',require('./routes/categories'));
 routes.use('/suppliers',require('./routes/suppliers'));
 routes.use('/orders',require('./routes/orders'));
 routes.use('/employees',require('./routes/employees'));
-routes.use('/product',require('./routes/product'));
+routes.use('/products',require('./routes/products'));
 // routes.use('/uploadDemo',require('./routes/uploadDemo'));
 
 // routes.use('/upload', require('./routes/upload'))
