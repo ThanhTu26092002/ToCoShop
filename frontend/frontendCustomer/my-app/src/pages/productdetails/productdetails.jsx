7b3740ef0a19bea6deb55ea0916324350c423a95
luunguyentrongnghia <luutrongnghia281102@gmail.com>
2022-12-14 13:04:33 +0700

    add

---
+++ frontend/frontendCustomer/my-app/src/pages/productdetails/productdetails.jsx
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from "react";
+import Slider from "../../components/slide/Slider"
+import Images from "../../components/Listproducts/images"
+import Footer from "../../components/Footer/Footer"
+import Search_cart from "../../components/SearchCart/index"
+import axios from 'axios';
+import "./productdetails.css"
+import { DownOutlined, UpOutlined } from '@ant-design/icons';
+import { InputNumber } from 'antd';
+function productdetails({ detall }) {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [images, setImages] = useState([]);
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+
+    axios.get("http://localhost:9000/categories3/").then((response) => {
+
+      setImages(response.data.results);
+    });
+
+
+  }, [])
+  return (
+    <div className='main_product_detall'>
+      <Slider />
+      <Search_cart />
+      <div className="detail">
+
+        <div className="product_detall_container">
+          <div className="product_img">
+            <div className="product_img_container">
+              <img src="newfashion11.png" alt="" />
+            </div>
+          </div>
+          <div className="product_detall">
+            <div className="product_detall_main">
+              <p>QUẦN THUN W2AT</p>
+              <div className="price">
+                <p><del>425.000VNĐ</del></p>
+                <p>375.000VNĐ</p>
+                <div className="discount">
+                  <p>giảm 25%</p>
+                </div>
+              </div>
+              <div className="size">
+                <p>SIZE: </p>
+                <div className="size_container">
+                  <a href="#">S</a>
+                  <a href="#">M</a>
+                  <a href="#">L</a>
+                </div>
+              </div>
+              <a href="#" className="btnsize">
+                Bảng qui đổi kích cở
+              </a>
+              <div className="amount">
+                <h4>Amount:</h4>
+                <InputNumber style={{ marginRight: 30 }} defaultValue={0} />
+                <p>1038 sản phẩm có sẵn</p>
+              </div>
+              <div className="btn_cart_buy">
+                <a href="#" className="btncart">
+                  THÊM VÀO GIỎ HÀNG
+                </a>
+                <a href="#" className="btnbuy">
+                  MUA NGAY
+                </a>
+              </div>
+              <ul>
+                <li><img src="image50.png"/><span>BẢO HÀNH TRONG VÒNG 90 NGÀY</span></li>
+                <li><img src="image50.png"/><span>ĐỔI HÀNG TRONG VÒNG 30 NGÀY</span></li>
+                <li><img src="image50.png"/><span>HOTLINE BÁN HÀNG: 0914 444 179</span></li>
+              </ul>
+
+            </div>
+
+          </div>
+        </div>
+        <div className="description">
+          <p>HÀNG TOÀN BỘ CỦA SHOP ĐỀU CÓ SẴN </p>
+          <br />
+          <p>ÁO PHÔNG basic trơn trắng, đen....</p>
+          <p>Chất liệu thun cotton mềm mịn, form chuẩn. 2 màu hồng, đỏ...</p>
+          <br />
+          <p> 149 Bà Triệu - Huế</p>
+          <p> Xuất xứ: Việt Nam</p>
+          <p>  Hot line : 01222773986</p>
+          <p> Link facebook shop: https://m.facebook.com/ToCoClothes</p>
+          <p> https://m.facebook.com/ToCoClothes</p>
+          <br />
+          <p>Sản phẩm của shop đảm bảo chất lượng tuyệt đối. Chất liệu đẹp, form dáng chuẩn. Và đặc biệt shop chuyên những mặt hàng tầm trung cho đến cao cấp không bán những mặt hàng rẻ, kém chất lượng. Khách hàng khi order sẽ được ship COD nên nếu sản phẩm không làm hài lòng quý khách hàng thì có thể ko thanh toán và gửi trả lại.</p>
+          <br />
+          <p>Chân thành cảm ơn quý khách hàng đã ghé xem và theo dõi shop ạ.</p>
+        </div>
+        <div className="grid_item">
+          <div className="related-section-header">
+            <h3 className="related-section-header_title">
+              <span className="related-section-header_text">
+                SẢN PHẨM CÙNG DANH MỤC
+              </span>
+            </h3>
+          </div>
+          <Images amount={9} data={images} ></Images>
+        </div>
+
+      </div>
+      <Footer amount1={8}></Footer>
+    </div>
+  )
+}
+
+export default productdetails
