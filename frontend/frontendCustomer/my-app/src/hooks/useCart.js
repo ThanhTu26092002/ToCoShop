12fe2324b80e0cebf6a7278f72bd801ae4b5d2e7
ThanhTu26092002 <lethanhtu810@email.com>
2022-12-25 09:41:34 +0700

    ok

--- frontend/frontendCustomer/my-app/src/hooks/useCart.js
+++ frontend/frontendCustomer/my-app/src/hooks/useCart.js
@@ -1,5 +1,5 @@
 import create from "zustand";
-import { persist, devtools } from "zustand/middleware";
+import { persist, devtools } from "zustand/middleware ";
 const persistOption = {
   name: "cart-storage",
   //   getStarage: () => sessionStorage,
